0635b6699a6953595a96a0ee55827b8c588cedd6
byunn90 <kayhanturk90@Live.com>
2023-04-21 04:46:23 +1000

    cv not working when clicked to download

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 import "../styles/Navbar.css";
 
 function Footer({ currentPage, handlePageChange }) {
   return (
     <nav className="my-footer">
-      <div className="container d-flex justify-content-between align-items-center">
+      <div className="container d-flex justify-content-center align-items-center">
         <a className="navbar-brand" href="#">
           © 2023 Kayhan PortFolio | All rights reserved.
         </a>
+
+        <div className="social-icons ml-auto">
+          <a href="https://github.com/byunn90">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/kayhan-mamak-5423b4262/">
+            <FaLinkedin />
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100080817559358">
+            <FaFacebook />
+          </a>
+        </div>
       </div>
     </nav>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,33 @@
 import React from "react";
-import pfp from "../../images/photoofme.jpg";
+import pfp from "../../images/KayhanImage.jpg";
 import "../../styles/Home.css";
+
 function Home() {
   return (
     <section className="home">
       <div className="left">
-        <img src={pfp}></img>
+        <div className="image-container">
+          <img src={pfp} alt="Kayhan Mamak" className="portrait" />
+        </div>
       </div>
       <div className="right">
-        <p>Hi my name is Kayhan</p>
+        <div className="cloud-container">
+          <p className="about-me">
+            Hi, I'm Kayhan Mamak. As a full-stack developer, I am passionate
+            about building web applications using modern technologies. I am
+            particularly interested in the MERN stack and currently honing my
+            skills in React. I love using my creativity and attention to detail
+            to build unique and innovative solutions for my clients. In my free
+            time, I enjoy exploring new technologies and experimenting with new
+            coding techniques to improve my skills. Thank you for visiting my
+            portfolio, and I look forward to hearing from you!
+          </p>
+          <div>
+            <a href="../../CV/kayhanResume.pdf" download>
+              <button>Resume</button>
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
